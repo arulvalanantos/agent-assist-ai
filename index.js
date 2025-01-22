20582b5c94e24e2ce8fbce025eba3fe354c67c1e
arulvalananto <arulvalananto@gmail.com>
2025-01-22 20:06:07 +0530

    feat: update server port to 8081 and remove unnecessary padding from image styles

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.get('/ping', (req, res) => {
   res.send('pong');
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
